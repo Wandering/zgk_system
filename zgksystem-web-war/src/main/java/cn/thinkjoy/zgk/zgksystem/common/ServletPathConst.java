45e948f3695afcacded7f4a09c7a6f5d7f3c0056
liuxiaowen <liuxiaowen@thinkjoy.cn>
2016-03-29 20:28:09 +0800

    查询用户拿货代理商信息接口

---
+++ zgksystem-web-war/src/main/java/cn/thinkjoy/zgk/zgksystem/common/ServletPathConst.java
@@ -0,0 +1,16 @@
+package cn.thinkjoy.zgk.zgksystem.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by clei on 15/8/8.
+ */
+public class ServletPathConst {
+
+    public static List<String> MAPPING_URLS = new ArrayList<String>();
+
+    static{
+        MAPPING_URLS.add("/system/agent/getAgentInfo.do");
+    }
+}
