07a6e006d5865eeb33c33b167457e9e68a6c6231
douzi <douzy75952@hotmail.com>
2016-04-01 18:18:27 +0800

    层级分成

--- zgksystem-api/src/main/java/cn/thinkjoy/zgk/zgksystem/AgentService.java
+++ zgksystem-api/src/main/java/cn/thinkjoy/zgk/zgksystem/AgentService.java
@@ -1,7 +1,7 @@
 package cn.thinkjoy.zgk.zgksystem;
 
 import cn.thinkjoy.zgk.zgksystem.domain.Department;
-import cn.thinkjoy.zgk.zgksystem.pojo.SplitPricePojo;
+import cn.thinkjoy.zgk.zgksystem.domain.SplitPricePojo;
 
 import java.util.List;
 
