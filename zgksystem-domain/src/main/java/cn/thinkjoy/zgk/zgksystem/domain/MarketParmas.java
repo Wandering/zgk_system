bafaaed21d08533e3a414be5a09fa714c6e0b445
douzi <douzy75952@hotmail.com>
2016-04-12 10:00:00 +0800

    system api version 12

--- zgksystem-domain/src/main/java/cn/thinkjoy/zgk/zgksystem/domain/MarketParmas.java
+++ zgksystem-domain/src/main/java/cn/thinkjoy/zgk/zgksystem/domain/MarketParmas.java
@@ -11,13 +11,17 @@ public class MarketParmas extends CreateBaseDomain {
      */
     private Integer costPrice;
     /**
-     * 分成比例 购买用户的前两级
+     * 代理商分成比例
      */
     private Integer splitPercentage;
     /**
-     * 各层级利润
+     * 用户层级利润
      */
     private String levelProfits;
+    /**
+     * 层级
+     */
+    private byte splitLevel;
 
     public Integer getCostPrice() {
         return costPrice;
@@ -41,5 +45,13 @@ public class MarketParmas extends CreateBaseDomain {
 
     public void setLevelProfits(String levelProfits) {
         this.levelProfits = levelProfits;
+    }
+
+    public byte getSplitLevel() {
+        return splitLevel;
+    }
+
+    public void setSplitLevel(byte splitLevel) {
+        this.splitLevel = splitLevel;
     }
 }
