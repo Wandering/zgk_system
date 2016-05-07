0d639975ea5c0d3bcf88aa7b45545d88c5fc4d4e
dengshaofei <dengsf007@gmail.com>
2016-05-07 18:21:50 +0800

    commit 解决市 县区  新增提交问题

--- zgksystem-web-war/src/main/webapp/admin/static/scripts/index/dep/dep_list.js
+++ zgksystem-web-war/src/main/webapp/admin/static/scripts/index/dep/dep_list.js
@@ -343,7 +343,7 @@ define(function (require, exports, module) {
                                             case 3:
                                                 var curProvincesId = updateProvincesId.substring(0, 2) + "0000";
                                                 $('#dep_city_from').show();
-                                                $('#dep_provinces_from,#dep_county_from').hide();
+                                                $('#dep_provinces_from,#dep_county_from,#web-control-group,#wechat-control-group').hide();
                                                 $.getJSON('/system/dataDictionary/findCityList?token=' + token + '&provinceId=' + curProvincesId, function (res) {
                                                     console.log(res)
                                                     for (var i = 0; i < res.bizData.length; i++) {
@@ -355,7 +355,7 @@ define(function (require, exports, module) {
                                             case 4:
                                                 var curProvincesCookieId = updateProvincesId.substring(0, 4) + "00";
                                                 $('#dep_county_from').show();
-                                                $('#dep_provinces_from,#dep_city_from').hide();
+                                                $('#dep_provinces_from,#dep_city_from,#web-control-group,#wechat-control-group').hide();
                                                 // 市
                                                 $.getJSON('/system/dataDictionary/findCountyList?token=' + token + '&cityId=' + curProvincesCookieId, function (res) {
                                                     console.log(res)
