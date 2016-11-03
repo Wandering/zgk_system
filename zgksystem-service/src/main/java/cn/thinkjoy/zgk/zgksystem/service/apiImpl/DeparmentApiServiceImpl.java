9c5a337e25b5b1dfb906b39a4c2cb5255219d3c4
yangyongping <yangyongping@operationadmins-MacBook-Pro.local>
2016-11-03 15:58:32 +0800

    添加输出单张卡类型

--- zgksystem-service/src/main/java/cn/thinkjoy/zgk/zgksystem/service/apiImpl/DeparmentApiServiceImpl.java
+++ zgksystem-service/src/main/java/cn/thinkjoy/zgk/zgksystem/service/apiImpl/DeparmentApiServiceImpl.java
@@ -16,6 +16,7 @@ import cn.thinkjoy.zgk.zgksystem.service.department.ISaleProductService;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -57,11 +58,11 @@ public class DeparmentApiServiceImpl implements DeparmentApiService {
     }
 
     @Override
-    public List<Department> queryDepartmentsByAreaCode(String areaCode,int areaType,int currentPage,int pageSize) {
+    public List<Department> queryDepartmentsByAreaCode(String areaCode, int areaType, int currentPage, int pageSize) {
 
         String tmpAreaCode = "";
-        if(areaType != -1){
-            tmpAreaCode = StringUtils.substring(areaCode,0,2*areaType);
+        if (areaType != -1) {
+            tmpAreaCode = StringUtils.substring(areaCode, 0, 2 * areaType);
         }
 
         List<Department> departments = exDeparmentService.queryDepartmentsByAreaCode(
@@ -75,15 +76,15 @@ public class DeparmentApiServiceImpl implements DeparmentApiService {
     @Override
     public int getDepartmentCountByAreaCode(String areaCode, int areaType) {
         String tmpAreaCode = "";
-        if(areaType != -1){
-            tmpAreaCode = StringUtils.substring(areaCode,0,2*areaType);
+        if (areaType != -1) {
+            tmpAreaCode = StringUtils.substring(areaCode, 0, 2 * areaType);
         }
         return exDeparmentService.getDepartmentCountByAreaCode(tmpAreaCode);
     }
 
     @Override
     public Department queryDepartmentInfoByCode(long departmentCode) {
-        return (Department) iDepartmentService.findOne("departmentCode",departmentCode);
+        return (Department) iDepartmentService.findOne("departmentCode", departmentCode);
     }
 
     @Override
@@ -105,43 +106,84 @@ public class DeparmentApiServiceImpl implements DeparmentApiService {
 
         List<SaleProduct> saleProducts = saleProductService.findAll();
 
-        Map<String,Object> queryMap = Maps.newHashMap();
-        queryMap.put("areaCode",StringUtils.substring(areaId,0,2));
-        queryMap.put("status",0);
+        Map<String, Object> queryMap = Maps.newHashMap();
+        queryMap.put("areaCode", StringUtils.substring(areaId, 0, 2));
+        queryMap.put("status", 0);
         queryMap.put("roleType", UserRoleEnum.PROVICE_AGENT.getValue());
         Department department = (Department) iDepartmentService.queryOne(queryMap);
 
         List<DepartmentProductRelationPojo> relations = Lists.newArrayList();
-        for(SaleProduct product : saleProducts){
+        for (SaleProduct product : saleProducts) {
             // 忽略金榜题名套餐
-            if(product.getType() == ProductTypeEnum.JBTM.getValue()){
+            if (product.getType() == ProductTypeEnum.JBTM.getValue()) {
                 continue;
             }
 
             // 忽略非当前省的套餐
-            if(!"0".equals(product.getAreaId()) && product.getAreaId().indexOf(areaId) == -1){
+            if (!"0".equals(product.getAreaId()) && product.getAreaId().indexOf(areaId) == -1) {
                 continue;
             }
 
             DepartmentProductRelation relation = null;
-            if(department != null){
+            if (department != null) {
                 queryMap.clear();
-                queryMap.put("status",0);
-                queryMap.put("productId",product.getId());
-                queryMap.put("departmentCode",department.getDepartmentCode());
+                queryMap.put("status", 0);
+                queryMap.put("productId", product.getId());
+                queryMap.put("departmentCode", department.getDepartmentCode());
                 relation = (DepartmentProductRelation) departmentProductRelationService.queryOne(queryMap);
             }
             DepartmentProductRelationPojo relationPojo = new DepartmentProductRelationPojo();
 
-            if(department == null || relation == null){
+            if (department == null || relation == null) {
                 relation = convert2DepartmentProductRelation(product);
             }
-            BeanUtils.copyProperties(relationPojo,relation);
+            BeanUtils.copyProperties(relationPojo, relation);
             relationPojo.setIntro(product.getIntro());
             relations.add(relationPojo);
         }
         return relations;
     }
+
+    @Override
+    public DepartmentProductRelationPojo queryProductPriceByAreaId(String areaId, Integer productId) throws InvocationTargetException, IllegalAccessException {
+        /**
+         * 1.查询出所有产品
+         * 2.判断用户所在区域是否有省级代理商
+         * 3.没有:直接显示产品默认价格
+         * 4.有:查询省级代理商是否有代理产品
+         * 5.没有:默认价格
+         * 6.有:返回代理商定价
+         */
+
+        Map<String, Object> map = new HashedMap();
+        map.put("productId", productId);
+        map.put("areaId", areaId);
+        SaleProduct saleProduct = (SaleProduct) saleProductService.queryOne(map);
+
+        Map<String, Object> queryMap = Maps.newHashMap();
+        queryMap.put("areaCode", StringUtils.substring(areaId, 0, 2));
+        queryMap.put("status", 0);
+        queryMap.put("roleType", UserRoleEnum.PROVICE_AGENT.getValue());
+        Department department = (Department) iDepartmentService.queryOne(queryMap);
+
+        DepartmentProductRelation relation = null;
+        if (department != null) {
+            queryMap.clear();
+            queryMap.put("status", 0);
+            queryMap.put("productId", saleProduct.getId());
+            queryMap.put("departmentCode", department.getDepartmentCode());
+            relation = (DepartmentProductRelation) departmentProductRelationService.queryOne(queryMap);
+        }
+        DepartmentProductRelationPojo relationPojo = new DepartmentProductRelationPojo();
+
+        if (department == null || relation == null) {
+            relation = convert2DepartmentProductRelation(saleProduct);
+        }
+        BeanUtils.copyProperties(relationPojo, relation);
+        relationPojo.setIntro(saleProduct.getIntro());
+        return relationPojo;
+    }
+
 
     /**
      * 对象转换  SaleProduct --> DepartmentProductRelation
@@ -149,7 +191,7 @@ public class DeparmentApiServiceImpl implements DeparmentApiService {
      * @param product
      * @return
      */
-    private DepartmentProductRelation convert2DepartmentProductRelation(SaleProduct product){
+    private DepartmentProductRelation convert2DepartmentProductRelation(SaleProduct product) {
         DepartmentProductRelation relation = new DepartmentProductRelation();
         relation.setSalePrice(product.getDefaultSalePrice());
         relation.setProductName(product.getProductName());
